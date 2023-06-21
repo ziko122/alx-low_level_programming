cd8b48ec250cc107ab36322adac4fbdee72f9f2c
ziko122 <zakariaahoudig0@gmail.com>
2023-06-20 18:53:28 -0700

    first idea

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,13 +8,13 @@
 
 void print_alphabet(void)
 {
-char letter;
+	char letter;
 
 
-for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
 
 
-_putchar('\n');
+	_putchar('\n');
 }
 
